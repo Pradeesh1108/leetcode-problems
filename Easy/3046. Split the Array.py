8059f15bd69831a83e5440e8d79ec62bb5950f9b
Pradeesh S <154534896+Pradeesh1108@users.noreply.github.com>
2025-02-19 21:41:39 +0530

    Create 3046. Split the Array.py

---
+++ Easy/3046. Split the Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPossibleToSplit(self, nums: List[int]) -> bool:
+        count = Counter(nums)
+        for i in count.values():
+            if i > 2:
+                return False
+        return True
+        
