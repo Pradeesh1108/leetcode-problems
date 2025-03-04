41dcbed7828105f2d04ac818f170c3bdc0d2ffb6
Pradeesh S <154534896+Pradeesh1108@users.noreply.github.com>
2025-03-04 20:38:35 +0530

    Create 238. Product of Array Except Self.py

---
+++ Medium/238. Product of Array Except Self.py
@@ -0,0 +1,20 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        left_mult = 1
+        right_mult = 1
+
+        n = len(nums)
+
+        l_arr = [0] * n
+        r_arr = [0] * n
+
+        for i in range(n):
+            j = -i - 1
+            l_arr[i] = left_mult
+            r_arr[j] = right_mult
+
+            left_mult *= nums[i]
+            right_mult *= nums[j] 
+
+        return [left*right for left,right in zip(l_arr,r_arr)]       
