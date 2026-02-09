68bad4c6f660d8cb14da37801c20ad67d59c04cc
Pradeesh1108 <pradeeshsivaprakasam@gmail.com>
2026-02-09 12:33:31 +0530

    added a question to nference

---
+++ nferenceDrive/topKfrequent elements.py
@@ -0,0 +1,23 @@
+"""
+    This solution uses a bucket sort approach to find the top K frequent elements in a list of integers.
+"""
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = {}
+
+        for n in nums:
+            counter[n] = 1 + counter.get(n, 0)
+        
+        freq = [[] for _ in range(len(nums) + 1)]
+
+        for n, f in counter.items():
+            freq[f].append(n)
+        
+        res = []
+
+        for i in range(len(freq) - 1, -1, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
